b623ce762884507b32663a21e95de53cc2ada2e2
a8anassis <a8anassis@gmail.com>
2019-01-05 23:54:42 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package gr.aueb.elearn.chap1;
+
+// Το πρώτο πρόγραμμα Java - by thanassis
+/*
+
+Το πρόγραμμα αυτό εκτυπώνει στην οθόνη (std output)
+το αλφαριθμητικό Hello World!
+
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println();
+        System.out.printf("Hello Alice!\n");
+    }
+}
+
---
+++ MaxMin.java
@@ -0,0 +1,19 @@
+package gr.aueb.elearn.chap1;
+
+public class MaxMin {
+
+    public static void main(String[] args) {
+        System.out.println("Max int = " + Integer.MAX_VALUE);
+        System.out.println("Min int = " + Integer.MIN_VALUE);
+
+        System.out.println("Max long = " + Long.MAX_VALUE);
+        System.out.println("Min long = " + Long.MIN_VALUE);
+
+        System.out.println("Max short = " + Short.MAX_VALUE);
+        System.out.println("Min short = " + Short.MIN_VALUE);
+
+        System.out.println("Max byte = " + Byte.MAX_VALUE);
+        System.out.println("Min byte = " + Byte.MIN_VALUE);
+    }
+
+}
