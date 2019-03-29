c3ff50d6968a9e5e4a53d98ffef49c0316299238
Th. Androutsos <a8anassis@gmail.com>
2019-03-29 23:23:06 +0200

    Typecast example byteShortInt

---
+++ ByteShortInt.java
@@ -0,0 +1,15 @@
+package gr.thanassis.tmp;
+
+/**
+ * Created by thanos on 10/4/2018.
+ */
+public class ByteShortInt {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        System.out.println("i+j = " + (i + j));
+        System.out.printf("i+j = %04d", i+j);
+    }
+
+
+}
